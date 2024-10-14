30597fa5f363848a1d1cd1431b60533b0f461447
datag <sk3338@njit.edu>
2024-10-13 22:48:25 -0400

    initial commit

---
+++ src/data/resume/degrees.js
@@ -0,0 +1,16 @@
+const degrees = [
+  {
+    school: 'New Jersey Institute of Technology',
+    degree: 'M.S. Data Science',
+    link: 'https://www.njit.edu/',
+    year: 2022,
+  },
+  {
+    school: 'Sreyas Institute of Engineering and Technology',
+    degree: 'B. Tech. Electronics and Communication Engineering',
+    link: 'https://sreyas.ac.in/',
+    year: 2017,
+  },
+];
+
+export default degrees;
