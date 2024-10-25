dae47ed29e14f8a9d74663402cee129a4de184ba
Shashank Reddy Kallem <43008325+shashankreddy151@users.noreply.github.com>
2024-10-25 17:38:19 -0400

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  moduleNameMapper: {
-    '^.+\\.(css|less|scss)$': 'identity-obj-proxy', // Use identity-obj-proxy for CSS modules
-    '^.+\\.md$': 'markdown-to-jsx',
-  },
-};
-
-module.exports = config;
