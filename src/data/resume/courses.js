614a93397406c0068310c69e23c5e76a7c201b3f
datag <sk3338@njit.edu>
2024-10-27 00:26:59 -0400

    first deploy

---
+++ src/data/resume/courses.js
@@ -0,0 +1,64 @@
+const courses = [
+  {
+    title: 'Applied Statistics',
+    number: 'MATH 611',
+    link: 'https://math.njit.edu/sites/math/files/Math%20661-111%20F22.pdf',
+    university: 'NJIT',
+  },
+  {
+    title: 'Deep Learning',
+    number: 'DS 677',
+    link: 'https://digitalcommons.njit.edu/cgi/viewcontent.cgi?article=1393&context=cs-syllabi',
+    university: 'NJIT',
+  },
+  {
+    title: 'Machine Learning',
+    number: 'DS 675',
+    link: 'https://digitalcommons.njit.edu/cgi/viewcontent.cgi?article=1013&context=ds-syllabi',
+    university: 'NJIT',
+  },
+  {
+    title: 'Introduction to Big Data',
+    number: 'DS 644',
+    link: 'https://digitalcommons.njit.edu/cgi/viewcontent.cgi?article=1389&context=cs-syllabi',
+    university: 'NJIT',
+  },
+  {
+    title: 'Artificial Intelligence',
+    number: ' CS 670',
+    link: 'https://digitalcommons.njit.edu/cgi/viewcontent.cgi?article=1455&context=cs-syllabi',
+    university: 'NJIT',
+  },
+  {
+    title: 'Data Analytics with R Program',
+    number: 'CS 636',
+    link: 'https://digitalcommons.njit.edu/cgi/viewcontent.cgi?article=1157&context=cs-syllabi',
+    university: 'NJIT',
+  },
+  {
+    title: 'Data Mining',
+    number: 'CS 634',
+    link: 'https://web.njit.edu/~hseuming/CS-634.html',
+    university: 'NJIT',
+  },
+  {
+    title: 'Web Systems Development',
+    number: 'IS 601',
+    link: 'https://digitalcommons.njit.edu/cgi/viewcontent.cgi?params=/context/info-syllabi/article/1130/&path_info=IS_601_852_202010.pdf',
+    university: 'NJIT',
+  },
+  {
+    title: 'Data Analytics for Info System',
+    number: 'IS 665',
+    link: 'https://digitalcommons.njit.edu/cgi/viewcontent.cgi?article=1202&context=info-syllabi',
+    university: 'NJIT',
+  },
+  {
+    title: 'Data Management System Design',
+    number: 'CS 631',
+    link: 'https://web.njit.edu/~wangj/631-summer-syllabus.pdf',
+    university: 'NJIT',
+  },
+];
+
+export default courses;
