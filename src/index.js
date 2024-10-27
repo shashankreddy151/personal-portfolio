08ece84a630717a7d8f74cf3936d18c769f43c1a
datag <sk3338@njit.edu>
2024-10-27 01:12:08 -0400

    adding initial react app

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// See https://reactjs.org/docs/strict-mode.html
-const StrictApp = () => (
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-const rootElement = document.getElementById('root');
-
-// hydrate is required by react-snap.
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, <StrictApp />);
-} else {
-  const root = createRoot(rootElement);
-  root.render(<StrictApp />);
-}
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
