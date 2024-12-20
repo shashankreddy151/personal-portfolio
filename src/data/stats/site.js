c0524c15c7cdd2c9e714311a5cf3ca919d1161f9
datag <sk3338@njit.edu>
2024-12-19 19:07:21 -0600

    dfd

--- src/data/stats/site.js
+++ src/data/stats/site.js
@@ -1,5 +1,6 @@
-import dayjs from 'dayjs';
+import dayjs from 'dayjs'; // Import dayjs for date formatting
 
+// Static GitHub response data for initial display
 const githubResponse = {
   id: 865166819,
   node_id: "R_kgDOM5Fl4w",
@@ -15,54 +16,54 @@ const githubResponse = {
   subscribers_count: 1,
   forks_count: 0,
   open_issues_count: 0,
-  pushed_at: "2024-10-28T21:49:06Z",
+  pushed_at: "2024-10-28T21:49:06Z", // ISO date format
 };
 
-// Extract relevant data from the GitHub response
+// Extract relevant data from the GitHub response and apply transformations
 const data = [
   {
     label: 'Stars this repository has on GitHub',
     key: 'stargazers_count',
-    value: githubResponse.stargazers_count || 0,
+    value: githubResponse.stargazers_count || 0, // Default to 0 if undefined
     link: `${githubResponse.html_url}/stargazers`,
   },
   {
     label: 'Number of people watching this repository',
     key: 'subscribers_count',
-    value: githubResponse.subscribers_count || 0,
+    value: githubResponse.subscribers_count || 0, // Default to 0 if undefined
     link: `${githubResponse.html_url}/stargazers`,
   },
   {
     label: 'Number of forks',
     key: 'forks_count',
-    value: githubResponse.forks_count || 0,
+    value: githubResponse.forks_count || 0, // Default to 0 if undefined
     link: `${githubResponse.html_url}/network`,
   },
   {
     label: 'Number of spoons',
-    value: '0',
+    value: '0', // This is a placeholder for humorous purposes
   },
   {
     label: 'Number of linter warnings',
-    value: '0',
+    value: '0', // Default value for linter warnings
   },
   {
     label: 'Open GitHub issues',
     key: 'open_issues_count',
-    value: githubResponse.open_issues_count || 0,
+    value: githubResponse.open_issues_count || 0, // Default to 0 if undefined
     link: `${githubResponse.html_url}/issues`,
   },
   {
     label: 'Last updated at',
     key: 'pushed_at',
-    value: dayjs(githubResponse.pushed_at).format('MMMM DD, YYYY'), // Format date
-    link: `${githubResponse.html_url}/commits`,
+    value: dayjs(githubResponse.pushed_at).format('MMMM DD, YYYY'), // Format the ISO date into "Month Day, Year"
+    link: `${githubResponse.html_url}/commits`, // Link to the GitHub commits page
   },
   {
     label: 'Lines of JavaScript powering this website',
-    value: '2150',
-    link: `${githubResponse.html_url}/graphs/contributors`,
+    value: '2150', // Static value for JavaScript lines
+    link: `${githubResponse.html_url}/graphs/contributors`, // Link to contributors page
   },
 ];
 
-export default data;
+export default data; // Export the processed data
