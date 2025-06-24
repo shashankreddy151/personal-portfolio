ab1cd4b476f64a33195f05cfd7792d6780328a41
datag <sk3338@njit.edu>
2025-06-24 01:19:22 -0500

    new site

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: [], // Add domains if you use external images
-  },
-};
-
-module.exports = nextConfig;
--- src/lib/leaflet.ts
+++
@@ -1,1 +0,0 @@
-// Remove all code, as the icon fix and L import will be handled in TravelMap.tsx to avoid SSR issues.
